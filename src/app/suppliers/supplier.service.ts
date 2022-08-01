9954f67b1e147c39a505169e70bbdd2e56bd4a9c
Soumyak1303 <soumyak1303@gmail.com>
2022-08-01 12:39:35 +0530

    HOO with concatMap. Implementing supplier logic

--- src/app/suppliers/supplier.service.ts
+++ src/app/suppliers/supplier.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { throwError, Observable } from 'rxjs';
+import { throwError, Observable, of, map, tap, concatMap } from 'rxjs';
+import { Supplier } from './supplier';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  supplierWithMap$ = of(1, 5, 8).pipe(
+    // map((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    tap((id) => console.log(`concatmap o/p obs: ${id}`)),
+    concatMap((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+
+  constructor(private http: HttpClient) {
+    this.supplierWithMap$.subscribe((item) =>
+      console.log(`concatMap res: `, item)
+    );
+
+    // this.supplierWithMap$.subscribe(
+    //   (osub) => osub.subscribe((item) => console.log(`map result:`, item)) //example of inner and outter subscribe --> HOO
+    // );
+  }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
     // in a real world app, we may send the server to some remote logging infrastructure
@@ -26,5 +41,4 @@ export class SupplierService {
     console.error(err);
     return throwError(() => errorMessage);
   }
-
 }
